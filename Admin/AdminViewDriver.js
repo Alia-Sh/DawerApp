661485c4835828dbafe5b5fc24230279ba3603b9
HussaSq <hessahsaud10@gmail.com>
2020-11-24 22:08:49 +0300

    Update AdminViewDriver.js

--- Admin/AdminViewDriver.js
+++ Admin/AdminViewDriver.js
@@ -1,15 +1,55 @@
 import React, {useEffect,useState}from 'react';
-import { StyleSheet, Text, View,Image,Platform,Linking}from 'react-native';
+import { StyleSheet, Text, View,Image,Platform,Linking,FlatList,TouchableOpacity}from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'; 
 import {Title,Card,Button,FAB}from 'react-native-paper';
 import Feather from 'react-native-vector-icons/Feather';
 import { NativeModules } from 'react-native';
 import firebase from '../Database/firebase';
-import {FontAwesome5} from '@expo/vector-icons';
+import {FontAwesome5,MaterialIcons} from '@expo/vector-icons';
 import { YellowBox } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ArabicNumbers } from 'react-native-arabic-numbers';
 //import DriverHome from './DriverHome';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 YellowBox.ignoreWarnings(['Setting a timer']);
+
+
+const Item = ({ item, onPress, style }) => (
+  
+    <TouchableOpacity onPress={onPress} style={[styles.item, style,{flex:1}]}>
+      <View  style={[styles.flexDirectionStyle,{height:45,flex:1}]}>
+        
+      <Image
+            source={require('../assets/AdminIcons/requestIcon.jpg')}
+            style={{width:40,height:40,marginRight:-5,borderRadius:12}}
+            resizeMode="stretch"
+            />
+       
+        <View style={{marginTop:Platform.OS === 'android'? -8:0,paddingLeft:10}} >
+          <Text style={[styles.Status,{textAlign: Platform.OS === 'android' && 
+            NativeModules.I18nManager.localeIdentifier === 'ar_EG' || 
+            NativeModules.I18nManager.localeIdentifier === 'ar_AE' ||
+            NativeModules.I18nManager.localeIdentifier === 'ar_SA'? 'left':'right'}]}>طلب  </Text>
+          <Text style={styles.date}>وقت الإستلام : {item.Date}</Text>
+          
+          
+        </View>
+        <View style={{flex:1}}>
+  
+          <View style={{marginRight:'15%',marginLeft:'5%',marginTop:5,alignItems:'flex-start'}}>
+              <MaterialIcons 
+                  name="error" 
+                  size={30} 
+                  color="#7B7B7B"
+              />
+          </View>
+        </View>
+      
+       
+      </View>
+    </TouchableOpacity>
+   
+  );
 
 const AdminViewDriver = ({navigation,route})=>{
 
@@ -47,6 +87,8 @@ const AdminViewDriver = ({navigation,route})=>{
 
     useEffect(()=>{
         retriveImage()
+        fetchData()
+
     },[]);
 
     const openDial=(phone)=>{
@@ -59,8 +101,85 @@ const AdminViewDriver = ({navigation,route})=>{
     // const updateSecureTextEntry=()=>{
     //     setSecureTextEntry(!SecureTextEntry)
     //   }
-    return(
 
+    // list
+    
+    const [RequestList,setRequestList] = useState([])
+    const[loading,setLoading]=useState(true)
+    
+const fetchData=()=>{
+  firebase.database().ref('/PickupRequest/').on('value',snapshot=>{
+    const Data = snapshot.val();
+    console.log(Data);
+    if(Data){
+      var li = []
+        snapshot.forEach(function(snapshot){
+        console.log(snapshot.key);
+        console.log(snapshot.val());
+        var User=snapshot.key
+        firebase.database().ref('/PickupRequest/'+User).on('value',snapshot=>{
+           // setUser(UserId)
+          snapshot.forEach(function(snapshot){
+            console.log(snapshot.key);
+            console.log(snapshot.val().Status);
+            if(snapshot.val().DeliveryDriverId == userId ){
+                
+              var temp={Date:snapshot.val().DateAndTime,
+                key:snapshot.key,
+                Status:snapshot.val().Status,
+                UserId:User,
+            }
+            //console.log(n+'check again ');
+              li.push(temp)
+              setLoading(false)
+            }
+            
+          })
+        })
+      })
+      if(li){
+         //HERE to sort the list depending on the date
+      li.sort(function(a, b){
+        return new Date(a.Date) - new Date(b.Date);
+      });
+    
+    }
+      setRequestList(li)
+
+      console.log(li)
+    }
+  })
+}
+
+  
+
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+  //const backgroundColor = item.key === selectedId ? "#EDEEEC" : "#F3F3F3";
+
+      return (
+        <Item
+          item={item}
+          // onPress={() => setSelectedId(item.key)}
+          
+          onPress={() => 
+          { var ID =item.key;
+            var DATE=item.Date;
+            var STATUS = item.Status;
+            var UserId=item.UserId;
+            console.log(ID+'      >>>>>here in gome');
+            navigation.navigate("DriverRequestDetails",{ID,DATE,STATUS,UserId})}}
+          style={{ backgroundColor :item.key === selectedId ? "#EDEEEC" : "#F3F3F3"}}
+        />
+      );
+      };
+
+    //end of list
+
+
+    return(
+    <KeyboardAwareScrollView>
         <View style={styles.root}>
             <SafeAreaView style={{flexDirection:'row-reverse'}}>
                 <View style={styles.header}>
@@ -135,6 +254,9 @@ const AdminViewDriver = ({navigation,route})=>{
                       
                             <View style={{alignItems:"center",margin:15}}>
                            <Title style={{color: '#9E9D24',fontSize:20}}>الطلبات المسندة</Title>
+
+                           <Title style={[styles.text,{marginTop:-5,marginBottom:3}]}
+       > عدد الطلبات  : {ArabicNumbers(RequestList.length)}</Title>
                            
                        </View>
                        <Image
@@ -142,12 +264,31 @@ const AdminViewDriver = ({navigation,route})=>{
                             source={require('../assets/line.png')}
                             />
                 </View>
+                
+                <View>
+                    
+      
+    
+            <FlatList
+              data={RequestList}
+              renderItem={renderItem}
+              keyExtractor={(item)=>item.key}
+              extraData={selectedId}
+              onRefresh={()=>fetchData()}
+              refreshing={loading}
+            />
+          
+            {/* end request list*/ }
+                    
+                </View>
+                
 
                
 
             </View>  
 
         </View>
+        </KeyboardAwareScrollView> 
     );
 }
 
@@ -228,6 +369,82 @@ const styles=StyleSheet.create({
     icon2:{
         position: 'absolute',
         right: 16
-    }
+    },
+    text: {
+        // color: '#809d65',
+         fontSize: 14,
+         textAlign: 'center',
+         marginRight:9,
+         marginLeft:9,
+     },
+     item: {
+        backgroundColor: '#F3F3F3',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius :10,
+        shadowColor :'#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 5,
+        padding :15,
+      },
+      title: {
+        fontSize: 18,
+        fontWeight: 'bold' ,
+        textAlign :'right',
+        marginRight:30,
+        marginTop:10, 
+      },
+      user: {
+        fontSize: 12,
+        textAlign :'right',
+        marginRight:30,
+        marginTop:5,
+        color :'#ADADAD',
+      },
+      Status: {
+        fontSize: 18,
+        fontWeight: 'bold' ,
+        textAlign :'right',
+        marginRight:30,
+        marginTop:5, 
+      },
+      date: {
+        fontSize: 14,
+        textAlign :'right',
+        marginRight:30,
+        marginTop:5,
+        color :'#7B7B7B',
+      },
+      fabIOS: {
+        position: 'absolute',
+        margin: 16,
+        left: 0,
+        bottom: 0,
+      },
+      fabAndroid: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+      },
+      icon:{
+        position: 'absolute',
+        marginTop:30,
+        left: 16
+      },
+      flexDirectionStyle:{
+        flexDirection: Platform.OS === 'android' && 
+        NativeModules.I18nManager.localeIdentifier === 'ar_EG' || 
+        NativeModules.I18nManager.localeIdentifier === 'ar_AE' ||
+        NativeModules.I18nManager.localeIdentifier === 'ar_SA'? 'row' : 'row-reverse',  
+      } ,
+    //end flat list
+  
 })
 export default AdminViewDriver
